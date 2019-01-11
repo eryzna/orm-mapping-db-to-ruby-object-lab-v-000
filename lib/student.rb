e8cd7556b677ee4d1decc0ab3da63867948c1ffc
Elana Ryznar <eryzna@gmail.com>
2019-01-11 00:32:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -106,5 +106,17 @@ class Student
     end.first
   end
 
+  def self.all_students_in_grade_X (grade_number)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade=?
+    SQL
+
+    DB[:conn].execute(sql, grade_number).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 
 end
